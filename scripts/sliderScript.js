a000abf9545fcd3109aeea5b252d35facc419a3a
markrogers95 <mark_199518@hotmail.co.uk>
2020-05-18 20:41:07 +0100

    Started contact

--- scripts/sliderScript.js
+++ scripts/sliderScript.js
@@ -1,66 +1,66 @@
-$('.slider').each(function() {              // For every slider
-    var $this   = $(this);                    // Current slider
-    var $group  = $this.find('.slide-group'); // Get the slide-group (container)
-    var $slides = $this.find('.slide');       // Create jQuery object to hold all slides
-    var buttonArray  = [];                    // Create array to hold navigation buttons
-    var currentIndex = 0;                     // Hold index number of the current slide
-    var timeout;                              // Sets gap between auto-sliding
+$('.slider').each(function() {
+    var $this   = $(this);
+    var $group  = $this.find('.slide-group');
+    var $slides = $this.find('.slide');
+    var buttonArray  = [];
+    var currentIndex = 0;
+    var timeout;
   
-    function move(newIndex) {          // Creates the slide from old to new one
-      var animateLeft, slideLeft;      // Declare variables
+    function move(newIndex) {
+      var animateLeft, slideLeft;
   
-      advance();                       // When slide moves, call advance() again
+      advance();
   
-      // If it is the current slide / animating do nothing
+      
       if ($group.is(':animated') || currentIndex === newIndex) {  
         return;
       }
   
-      buttonArray[currentIndex].removeClass('active'); // Remove class from item
-      buttonArray[newIndex].addClass('active');        // Add class to new item
+      buttonArray[currentIndex].removeClass('active');
+      buttonArray[newIndex].addClass('active');
   
-      if (newIndex > currentIndex) {   // If new item > current
-        slideLeft = '100%';            // Sit the new slide to the right
-        animateLeft = '-100%';         // Animate the current group to the left
-      } else {                         // Otherwise
-        slideLeft = '-100%';           // Sit the new slide to the left
-        animateLeft = '100%';          // Animate the current group to the right
+      if (newIndex > currentIndex) {
+        slideLeft = '100%';
+        animateLeft = '-100%';
+      } else {
+        slideLeft = '-100%';
+        animateLeft = '100%';
       }
-      // Position new slide to left (if less) or right (if more) of current
+      
       $slides.eq(newIndex).css( {left: slideLeft, display: 'block'} );
   
-      $group.animate( {left: animateLeft}, function() {    // Animate slides and
-        $slides.eq(currentIndex).css( {display: 'none'} ); // Hide previous slide      
-        $slides.eq(newIndex).css( {left: 0} ); // Set position of the new item
-        $group.css( {left: 0} );               // Set position of group of slides
-        currentIndex = newIndex;               // Set currentIndex to the new image
+      $group.animate( {left: animateLeft}, function() {
+        $slides.eq(currentIndex).css( {display: 'none'} );
+        $slides.eq(newIndex).css( {left: 0} ); 
+        $group.css( {left: 0} );
+        currentIndex = newIndex;
       });
     }
   
-    function advance() {                     // Used to set 
-      clearTimeout(timeout);                 // Clear previous timeout
-      timeout = setTimeout(function() {      // Set new timer
-        if (currentIndex < ($slides.length - 1)) { // If slide < total slides
-          move(currentIndex + 1);            // Move to next slide
-        } else {                             // Otherwise
-          move(0);                           // Move to the first slide
+    function advance() {
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        if (currentIndex < ($slides.length - 1)) {
+          move(currentIndex + 1);
+        } else {
+          move(0);
         }
-      }, 10000);                              // Milliseconds timer will wait
+      }, 10000);
     }
   
     $.each($slides, function(index) {
-      // Create a button element for the button
+      
       var $button = $('<button type="button" class="slide-btn">&bull;</button>');
-      if (index === currentIndex) {    // If index is the current item
-        $button.addClass('active');    // Add the active class
+      if (index === currentIndex) {
+        $button.addClass('active');
       }
-      $button.on('click', function() { // Create event handler for the button
-        move(index);                   // It calls the move() function
-      }).appendTo('.slide-buttons');   // Add to the buttons holder
-      buttonArray.push($button);       // Add it to the button array
+      $button.on('click', function() {
+        move(index);
+      }).appendTo('.slide-buttons');
+      buttonArray.push($button);
     });
   
-    advance();                          // Script is set up, advance() to move it
+    advance();
   
   
   });
