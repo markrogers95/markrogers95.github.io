7abbb10f0f6ee09c80d13216f6ff7875760040d0
markrogers95 <mark_199518@hotmail.co.uk>
2020-05-24 16:40:51 +0100

    Disabled form

--- contact/contactScript.php
+++ contact/contactScript.php
@@ -7,21 +7,17 @@ $errorurl = 'error.html' ;
 $success = 'success.html' ; 
 
 function remove_headers($string) {
-$headers = array(
-"/to\:/i",
-"/from\:/i",
-"/bcc\:/i",
-"/cc\:/i",
-"/Content\-Transfer\-Encoding\:/i",
-"/Content\-Type\:/i",
-"/Mime\-Version\:/i"
-);
-if (preg_replace($headers, '', $string) == $string) {
-return $string;
-} else {
-die('You are not completing the form correctly.');
+    $headers = array("/to\:/i","/from\:/i","/bcc\:/i","/cc\:/i",
+                    "/Content\-Transfer\-Encoding\:/i",
+                    "/Content\-Type\:/i", "/Mime\-Version\:/i");
+    if (preg_replace($headers, '', $string) == $string){
+        return $string;
+    }
+    else{
+        die('You are not completing the form correctly.');
+    }
 }
-}
+
 $uself = 0;
 $headersep = (!isset( $uself ) || ($uself == 0)) ? "\r\n" : "\n" ;
 
@@ -30,25 +26,26 @@ $name = remove_headers($_POST['name']) ;
 $subject = remove_headers($_POST['subject']) ;
 $message = remove_headers($_POST['message']) ;
 
-if (get_magic_quotes_gpc()) {
-$name = stripslashes( $name );
-$subject = stripslashes( $subject );
-$message = stripslashes( $message );
+if (get_magic_quotes_gpc()){
+    $name = stripslashes( $name );
+    $subject = stripslashes( $subject );
+    $message = stripslashes( $message );
 }
-if (!isset($_POST['email'])) {
-header( "Location: $formurl" ); 
+if (!isset($_POST['email'])){
+    header( "Location: $formurl" ); 
 }
-elseif ($_POST['catchbot'] !="") {
-die();
+elseif ($_POST['catchbot'] !=""){
+    die();
 }
-elseif ( preg_match( "[\r\n]", $name ) || preg_match( "[\r\n]", $email ) ) {
-header( "Location: $errorurl" );
-exit ;
+elseif ( preg_match( "[\r\n]", $name ) || preg_match( "[\r\n]", $email ) ){
+    header( "Location: $errorurl" );
+    exit;
 }
-else {
-mail( $admin, "Feedback: $subject", "$name\r\n$email\r\n$message", "From: $name <$admin>" );
-mail( $email, "Feedback: $subject", $replymsg , "From: $admin" );
-header( "Location: $success" );
+else{
+    mail( $admin, "Feedback: $subject", "$name\r\n$email\r\n$message", "From: $name <$admin>" );
+    mail( $email, "Feedback: $subject", $replymsg , "From: $admin" );
+    header( "Location: $success" );
 }
+
 ?>
 
